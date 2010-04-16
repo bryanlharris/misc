37ec3fec1be0a1a882cebcac1d92b2353214da09
Bryan Harris <bharris@neospire.net>
2010-04-16 10:39:31 -0500

    Pull nerds changes as well

--- testing/dns-dal1.sh
+++ testing/dns-dal1.sh
@@ -4,13 +4,16 @@ set -e
 exec 1>>/home/bharris/cron.log
 exec 2>>/home/bharris/cron.log
 
-# Simple pre-run checks
 [ `whoami` = "bharris" ]   # Make sure user is bharris
 ssh -n -T sr               # Make sure sr is alive on 22
 pgrep git && exit
 
-# Pull recent changes then push to shared repository
 ( cd ~/src/git/dns-dal1
   git svn rebase svn+ssh://svn@sr/svn/dns-dal1
+  git push shared --all
+)
+
+( cd ~/src/git/nerds
+  git svn rebase svn+ssh://svn@sr/svn/nerds
   git push shared --all
 )
